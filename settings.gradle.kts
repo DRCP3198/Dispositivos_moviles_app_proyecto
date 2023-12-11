b474d13c2922d8060d635b4c99240891abb6c598
Dillan Coloma <renatocoloma2010@gmail.com>
2023-12-10 22:43:10 -0500

    Login Maqueta Inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "reproductor_musica_dc"
+include(":app")
+ 
